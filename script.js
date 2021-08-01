a34b6b57334aedb8e29ad90c5cbe986bbe80aba1
Thomas-j1 <Thomas.johanness1@gmail.com>
2021-08-01 23:16:36 +0200

    Edited script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const numberOfBoxes = 16;
 const resetButton = document.querySelector('#reset');
 const opacitySlider = document.querySelector('#opacitySlider');
 
-let drawColor = 'rgba(0,0,0,128)';
+let drawColor = 'rgba(0,0,0,0.5)';
 let opacity = 0.5;
 
 for (let i = 0; i<numberOfBoxes; i++){
@@ -22,53 +22,46 @@ const boxes = document.querySelectorAll('.box');
 
 function onHover(e){
     let currentColor = getComputedStyle(this).backgroundColor;
-    console.log(currentColor);
-    let newColor = addColor(currentColor);
-    console.log(newColor);
+    let newColor = blendColors(currentColor);
     this.style.backgroundColor=newColor;
 }
 
-function addColor(currentColor){
-    //blended = front + back * (1.0 - front.a)
-    //outputRed = (foregroundRed * foregroundAlpha) + (backgroundRed * (1.0 - foregroundAlpha));
+function blendColors(currentColor){
+    /*
+    Short answer:
+    if we want to overlay c0 over c1 both with some alpha then
+
+    a01 = (1 - a0)·a1 + a0
+    r01 = ((1 - a0)·a1·r1 + a0·r0) / a01
+    g01 = ((1 - a0)·a1·g1 + a0·g0) / a01
+    b01 = ((1 - a0)·a1·b1 + a0·b0) / a01
+
+    Note that division by a01 in the formulas for the components of color. It's important.
+*/
 
     let backgroundColorArray = rgbaValuesToArray(currentColor);
     let drawColorArray = rgbaValuesToArray(drawColor);
-    let newColorArray = [];
 
-    console.log(drawColorArray.length);
+    r0 = parseFloat(drawColorArray[0]);
+    g0 = parseFloat(drawColorArray[1]);
+    b0 = parseFloat(drawColorArray[2]);
+    a0 = parseFloat(drawColorArray[3]);
 
-    for(let i = 0; i<drawColorArray.length-1; i++){
-        let newValue = parseFloat(drawColorArray[i]*opacity) + (parseFloat(backgroundColorArray[i]) *
-        (1.0-opacity));
-        console.log(newValue);
-        newColorArray.push(newValue);
-    }
-    console.log('old op: ' + ((parseFloat(backgroundColorArray[3]) + opacity)));
-    newColorArray.push(opacity);
-    console.log(newColorArray);
+    r1 = parseFloat(backgroundColorArray[0]);
+    g1 = parseFloat(backgroundColorArray[1]);
+    b1 = parseFloat(backgroundColorArray[2]);
+    a1 = parseFloat(backgroundColorArray[3]);
 
+    a01 = (1 - a0) *a1 + a0;
+    r01 = ((1 - a0) *a1*r1 + a0*r0) / a01;
+    g01 = ((1 - a0) *a1*g1 + a0*g0) / a01;
+    b01 = ((1 - a0) *a1*b1 + a0*b0) / a01;
+
+    let newColorArray = [r01, g01, b01, a01];
     let newColor = 'rgba(' + newColorArray.join() + ')';
     
     return newColor;
 }
-
-
-/*
-Short answer:
-
-if we want to overlay c0 over c1 both with some alpha then
-
-a01 = (1 - a0)·a1 + a0
-
-r01 = ((1 - a0)·a1·r1 + a0·r0) / a01
-
-g01 = ((1 - a0)·a1·g1 + a0·g0) / a01
-
-b01 = ((1 - a0)·a1·b1 + a0·b0) / a01
-
-Note that division by a01 in the formulas for the components of color. It's important.
-*/
 
 boxes.forEach(box => box.addEventListener('mouseover', onHover) );
 
@@ -94,7 +87,6 @@ colorButtons.forEach(button => button.addEventListener('click', changeColor));
 opacitySlider.addEventListener('change', () => {
 opacity = opacitySlider.value/100; 
 updateOpacity();
-console.log(opacity);
 });
 
 //change drawColor value at index with new one
@@ -104,7 +96,6 @@ function changeRGBA(newValue, oldIndex){
     rgbaArray[oldIndex] = newValue;
     console.log(rgbaArray);
     let newColor = 'rgba(' + rgbaArray.join() + ')';
-    console.log(newColor);
     drawColor = newColor;
 }
 
